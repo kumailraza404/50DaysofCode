3fd799efb882d4525fd49a293f5daee3e92e5808
kumailraza404 <kumailraza404@gmail.com>
2021-11-03 14:42:21 +0500

    dayy 2 finished

---
+++ day2/day2.js
@@ -0,0 +1,69 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function (arr) {
+  for (i = 0; i < arr.length - 1; i++) {
+    //iterate through arr
+    if (arr[i] == 0) {
+      // if 0 is found, iterate the array from j(last index) to i *backwards* and push values
+      for (j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
+      }
+      i++;
+    }
+  }
+};
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  nums2.forEach((el, index) => (nums1[m + index] = el));
+  nums1.sort((a, b) => a - b);
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  // let res = nums.filter((el) => el !== val);
+
+  // nums.map((el, index) =>
+  //     nums[index] = res[index]
+  // )
+
+  // return nums.length
+
+  for (i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+  return nums.length;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let count = 0;
+  // Loop for all the elements in the array
+  for (let i = 0; i < nums.length; i++) {
+    // If the current element is equal to the next element, we skip
+    if (nums[i] == nums[i + 1]) {
+      continue;
+    }
+    // We will update the array in place
+    nums[count] = nums[i];
+    count++;
+  }
+  return count;
+};
